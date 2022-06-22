076a58d014dac91b48ed5dc21bcda17dcb9ddf2d
QuYue <quyue1541@gmail.com>
2022-06-22 22:14:04 +0800

    Create some files.

---
+++ Code/Experiment_1/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   main.py
+@Time    :   2022/06/21 22:27:16
+@Author  :   QuYue 
+@Email   :   quyue1541@gmail.com
+@Desc    :   main
+'''
+
+# %% Import Package
+import numpy as np
---
+++ Code/Experiment_1/read/datasets.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   datasets.py
+@Time    :   2022/06/21 22:28:40
+@Author  :   QuYue 
+@Email   :   quyue1541@gmail.com
+@Desc    :   datasets
+'''
+
+

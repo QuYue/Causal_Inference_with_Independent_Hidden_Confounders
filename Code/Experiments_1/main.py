89411b569b7aba2ed3e15f14cc1864bdcd0c990e
QuYue <quyue1541@gmail.com>
2022-06-23 00:45:05 +0800

    Add .gitignore file.

--- Code/Experiments_1/main.py
+++ Code/Experiments_1/main.py
@@ -11,8 +11,41 @@
 # %% Import Package
 # Basic
 import os
-# 
-import read
+import random
+import torch
+import numpy as np
 
-# %%
-print(os.getcwd())
+# Add path
+os.chdir(os.path.dirname(__file__))
+
+# Self-defined
+import data
+
+# %% Set Super-parameters
+class PARAM():
+    def __init__(self) -> None:
+        # Dataset
+        self.dataset_name = "Synthetic"     # Dataset name
+
+        # Random seed
+        self.seed = 1           # Random seed
+        self.random_setting()   # Random seed setting
+
+    def random_setting(self):
+        # Setting of random seed
+        if self.seed is not None:
+            random.seed(self.seed)
+            np.random.seed(self.seed)
+            torch.manual_seed(self.seed)
+            torch.cuda.manual_seed(self.seed)
+            torch.cuda.manual_seed_all(self.seed)   # If you are using multi-GPU.
+
+
+        
+        
+Parm = PARAM()
+
+# %% Main Function
+if __name__ == "__main__":
+    print(1)
+    data.datasets.load(Parm.dataset_name)

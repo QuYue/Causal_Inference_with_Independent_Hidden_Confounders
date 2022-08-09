60d8dbd453a96c2d609a2a2c51175a3ff9c03ac1
quyue <quyue1541@gmail.com>
2022-08-09 20:12:27 +0800

    keep

--- Code/Experiments_1/main.py
+++ Code/Experiments_1/main.py
@@ -105,7 +105,6 @@ if __name__ == "__main__":
                 data = [data.to(Parm.device) for data in data]
                 data = dict(zip(Parm.dataset.keylist, data))
             Parm.recorder[cv, epoch] = record
-        # Parm.save(f"cv{cv}.json")
     Parm.save("final.json")
     print("Done!")
 
